65236fcb3f3de01e9c31ede11cba1518efc4909b
SaimonPhoenix <>
2021-06-30 11:19:49 +0500

    CHORE: SPPID test solution

---
+++ SPPIDPlaceSymbol/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace SPPIDPlaceSymbol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSPPIDPlaceSymbolTest = new System.Windows.Forms.Button();
+            this.tbSymbolPath = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSPPIDPlaceSymbolTest
+            // 
+            this.btnSPPIDPlaceSymbolTest.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSPPIDPlaceSymbolTest.Location = new System.Drawing.Point(329, 88);
+            this.btnSPPIDPlaceSymbolTest.Name = "btnSPPIDPlaceSymbolTest";
+            this.btnSPPIDPlaceSymbolTest.Size = new System.Drawing.Size(122, 23);
+            this.btnSPPIDPlaceSymbolTest.TabIndex = 0;
+            this.btnSPPIDPlaceSymbolTest.Text = "PIDPlaceSymbolTest";
+            this.btnSPPIDPlaceSymbolTest.UseVisualStyleBackColor = true;
+            this.btnSPPIDPlaceSymbolTest.Click += new System.EventHandler(this.btnSPPIDPlaceSymbolTest_Click);
+            // 
+            // tbSymbolPath
+            // 
+            this.tbSymbolPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSymbolPath.Location = new System.Drawing.Point(72, 18);
+            this.tbSymbolPath.Name = "tbSymbolPath";
+            this.tbSymbolPath.Size = new System.Drawing.Size(379, 20);
+            this.tbSymbolPath.TabIndex = 1;
+            this.tbSymbolPath.Text = "\\Equipment\\Mechanical\\Pumps\\Pump.sym";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tbSymbolPath, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnSPPIDPlaceSymbolTest, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(5);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(454, 114);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SymbolPath";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 114);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "SPPID PlaceSymbol Test";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSPPIDPlaceSymbolTest;
+        private System.Windows.Forms.TextBox tbSymbolPath;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SPPIDPlaceSymbol/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Llama;
+using Plaice;
+
+
+namespace SPPIDPlaceSymbol
+{
+    public partial class Form1 : Form
+    {
+        private string symbolPath;
+        
+        private LMADataSource datasource;
+        private _Placement placement;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            symbolPath = tbSymbolPath.Text;
+            datasource = new LMADataSource();
+        }
+
+        private void btnSPPIDPlaceSymbolTest_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(symbolPath))
+                throw new ArgumentNullException(nameof(symbolPath));
+
+            try
+            {
+                placement = new Placement();
+                LMSymbol symbol = placement.PIDPlaceSymbol(symbolPath, 0.183, 0.235);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+    }
+}
